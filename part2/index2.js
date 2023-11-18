547c4a7637d6666ac36095db3e91c818ab50ea47
SerovaKaterina <k.serova.k@yandex.ru>
2023-11-18 23:41:46 +0200

    add changes to part2

--- part2/index2.js
+++ part2/index2.js
@@ -5,20 +5,20 @@ const empty = document.getElementById('empty');
 //определяем количество оставшихся дней
 function countDays () {
     let birthday = document.getElementById('birthday').value;
+    let result = document.getElementById('result');
     let msBirthday = Date.parse(birthday);
     let today = new Date();
     let daysOnly = Math.floor((msBirthday - today) / (1000 * 60 * 60 * 24));//получаем количество дней
-    let result = document.getElementById('result');
     result.textContent = daysOnly;
-    info.style.visibility = "visible";
+    info.style.visibility = 'visible';
 //условия , если не заполнен инпут
     let blankInput = () => {
-    if(inputDate.value == "") {
-    empty.style.display = "block";
-    info.style.display = "none";
+    if(inputDate.value == '') {
+    empty.style.display = 'block';
+    info.style.display = 'none';
     } else {
-    empty.style.display = "none";
-    info.style.display = "block";
+    empty.style.display = 'none';
+    info.style.display = 'block';
     }
 };
     blankInput();
@@ -27,17 +27,14 @@ function countDays () {
     let lastDigit = daysOnly % 10;
     let secondLastDigit = Math.floor((daysOnly % 100) / 10);
     if(secondLastDigit === 1) {
-    days.textContent = "дней";
+    days.textContent = 'дней';
+    } else if(lastDigit >= 2 && lastDigit <= 4) {
+    days.textContent = 'дня';
+    } else if(lastDigit === 1) {
+        days.textContent = 'день';
     } else {
-    if(lastDigit >= 2 && lastDigit <= 4) {
-    days.textContent = "дня";
-    } else {
-    if(lastDigit === 1) {
-        days.textContent = "день";
-    } else {
-        days.textContent = "дней";
+        days.textContent = 'дней';
     }
     }
-    }
-}
+    
 button.addEventListener('click', countDays);
